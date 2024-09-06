8100f73eb9699d0cc0eb4a68e444630f9a4e1d7b
Lidor-sh <lidorschool3@gmail.com>
2024-09-06 13:34:24 +0300

    Using env

--- habit-tracker-server/database.py
+++ habit-tracker-server/database.py
@@ -1,8 +1,11 @@
 import sqlalchemy as sql
 import sqlalchemy.ext.declarative as declatative
 import sqlalchemy.orm as orm
+import dotenv
 
-DATABASE_URL = "postgresql://habituser:habit1234@localhost:5432/habitdb"
+vars = dotenv.dotenv_values(".env")
+
+DATABASE_URL = vars["DATABASE_URL"] # "postgresql://habituser:habit1234@localhost:5432/habitdb"
 
 
 engine = sql.create_engine(DATABASE_URL)
