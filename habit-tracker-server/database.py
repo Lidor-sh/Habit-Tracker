daaca999bdce989af3173def629510d1e5d223de
Lidor-sh <lidorschool3@gmail.com>
2024-09-06 14:22:27 +0300

    Successfully connected to railway host

--- habit-tracker-server/database.py
+++ habit-tracker-server/database.py
@@ -2,10 +2,12 @@ import sqlalchemy as sql
 import sqlalchemy.ext.declarative as declatative
 import sqlalchemy.orm as orm
 import dotenv
+import os
 
-vars = dotenv.dotenv_values(".env")
+dotenv.load_dotenv(".env")
 
-DATABASE_URL = vars["DATABASE_URL"] # "postgresql://habituser:habit1234@localhost:5432/habitdb"
+DATABASE_URL = os.getenv("DATABASE_PUBLIC_URL") # "postgresql://habituser:habit1234@localhost:5432/habitdb"
+print(DATABASE_URL)
 
 
 engine = sql.create_engine(DATABASE_URL)
